ce5308ece75eefd6f56332ad9833fe3af4c94db7
Joshie-Finster <77668881+Joshie-Finster@users.noreply.github.com>
2021-03-25 09:54:51 +0000

    connected database

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { mongo } = require('mongoose');
 const app = express();
-try {
-  mongoose = require('mongoose');
-} catch(e){
-  console.log(e);
-}
+const mongoose = require('mongoose')
+//mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
